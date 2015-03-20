9c6fa5bed71b0f6ccd94ff41911bd96827d09749
AhnSeongHyun <sh84.ahn@gmail.com>
2015-03-20 16:56:21 +0900

    modify install

--- install.py
+++ install.py
@@ -8,28 +8,15 @@ if str.lower(platform.uname()[0]) == "windows":
     is_windows = True
 
 
-def yellow(msg):
-    if is_windows:
-        return msg
-    else:
-        return "\033[1;33m" + msg + "\033[1;m"
+print "\nWelcome Project-Template."
+print "Start your Project"
 
-
-def red(msg):
-    if is_windows:
-        return msg
-    else:
-        return "\033[1;41m" + msg + "\033[1;m"
-
-print yellow("\nWelcome Project-Template.")
-print yellow("Start your Project")
-
-project_name = str(raw_input(yellow('\nTyping project name :')))
-print "what is project name? is" + red("\"" + project_name + "\"") + "."
+project_name = str(raw_input('\nTyping project name :'))
+print "what is project name? is" + "\"" + project_name + "\"" + "."
 
 project_path = "../" + project_name
 
-print "Rename project to  " + red("\"" + project_name + "\"") + "..."
+print "Rename project to  " +  "\"" + project_name + "\"" + "..."
 current_dir = os.path.abspath("./")
 dest_dir = os.path.join(os.path.dirname(current_dir), project_name)
 shutil.move(current_dir, dest_dir)
@@ -37,42 +24,33 @@ shutil.move(current_dir, dest_dir)
 
 shutil.move("./project.conf", "./" + project_name +".conf")
 
-with open("./project_base.py", "r") as f:
-    file = f.read()
-file = file.replace("project", project_name)
-with open("./" + project_name+"_base.py", "w") as f:
-    f.write(file)
-os.remove("./project_base.py")
+
+change_file_list = list()
+
+change_file_list.append({"filepath" : "./project_base.py", "is_remove" : True})
+change_file_list.append({"filepath" : "./views/project_view.py", "is_remove" : True})
+change_file_list.append({"filepath" : "./db/project_db.py", "is_remove" : True})
+change_file_list.append({"filepath" : "./api/project_api.py", "is_remove" : True})
+change_file_list.append({"filepath" : "./project.py", "is_remove" : True})
+change_file_list.append({"filepath" : "./project.ini", "is_remove" : True})
+change_file_list.append({"filepath" : "./start.sh", "is_remove" : False})
+change_file_list.append({"filepath" : "./stop.sh", "is_remove" : False})
 
 
-with open("./views/project_view.py", "r") as f:
-    file = f.read()
-file = file.replace("project", project_name)
-with open("./views/"+ project_name + "_view.py", "w") as f:
-    f.write(file)
-os.remove("./views/project_view.py")
-
-with open("./db/project_db.py", "r") as f:
-    file = f.read()
-file = file.replace("project", project_name)
-with open("./db/"+ project_name + "_db.py", "w") as f:
-    f.write(file)
-os.remove("./db/project_db.py")
-
-with open("./api/project_api.py", "r") as f:
-    file = f.read()
-file = file.replace("project", project_name)
-with open("./api/"+ project_name + "_api.py", "w") as f:
-    f.write(file)
-os.remove("./api/project_api.py")
+for file_info in change_file_list:
 
 
+    with open(file_info["filepath"], "r") as f:
+        file = f.read()
+    file = file.replace("project", project_name)
 
-with open("./main.py", "r") as f:
-    file = f.read()
-file = file.replace("project", project_name)
-with open("./main.py", "w") as f:
-    f.write(file)
+    if file_info["is_remove"]:
 
+        with open(file_info["filepath"].replace("project", project_name), "w") as f:
+            f.write(file)
+        os.remove(file_info["filepath"])
 
-print yellow("Complete. Enjoy Developing.")
+    else:
+        with open(file_info["filepath"], "w") as f:
+            f.write(file)
+print "Complete. Enjoy Developing."
